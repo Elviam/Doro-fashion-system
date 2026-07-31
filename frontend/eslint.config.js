f37254c56679e6436d7bdaa8c00923b20f88b3bf
Elvia <elviagtz268@gmail.com>
2026-07-30 20:34:13 -0600

    chore(lint): add React JSX usage support

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -13,12 +14,18 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     plugins: {
+      react,
       'react-hooks': reactHooks,
     },
-    rules: reactHooks.configs.recommended.rules,
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-vars': 'error',
+    },
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
     },
   },
 ])
